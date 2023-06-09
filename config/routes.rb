0e838022f73abe4cdb4a66868e28b1c4dde8d10c
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-06-09 08:13:47 +0000

    コントローラー設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'homes/top'
+  get 'homes/about'
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  devise_for :customers
+  devise_for :users, skip: [:passwords], controllers: {
+  registrations: "user/registrations",
+  sessions: 'user/sessions'
+}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
