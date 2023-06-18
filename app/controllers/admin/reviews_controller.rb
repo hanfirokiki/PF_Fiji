426ccbacb60cb278369174ae238974369e60171d
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-06-18 12:28:09 +0000

    ゲストも見れる一覧表示

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,15 +1,2 @@
 class Admin::ReviewsController < ApplicationController
-  def index
-  end
-
-  def show
-  end
-
-  def destroy
-    def destroy
-    @review = Review.find(params[:id])
-    @review.destroy
-      redirect_to root_path, notice: "レビューが削除されました。"
-    end
-  end
 end
