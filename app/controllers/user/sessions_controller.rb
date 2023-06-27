e5a6745dd001cf992d0daf894171bd21743aa125
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-06-27 05:43:51 +0000

    フラッシュ追加

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -31,7 +31,9 @@ class User::SessionsController < Devise::SessionsController
     ## アカウントを取得できなかった場合、このメソッドを終了する
     return if !@user
     ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
-    if @user.valid_password?(params[:user][:password])
+    if @user.valid_password?(params[:user][:password]) && (@user.is_deleted == true )
+      flash[:notice] = "退会済みです。再度登録をしてご利用ください"
+      redirect_to new_user_registration_path
     ## 【処理内容3】
     end
     end
