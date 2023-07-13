a97d5ae84931589dd3cdba742fbe4ca25cdefb14
Shoko Fukuda <128126234+hanfirokiki@users.noreply.github.com>
2023-07-13 12:02:36 +0000

    ドメイン取得

--- app/controllers/user/likes_controller.rb
+++ app/controllers/user/likes_controller.rb
@@ -3,13 +3,14 @@ class User::LikesController < ApplicationController
     @review = Review.find(params[:review_id])
     like = current_user.likes.new(review_id: @review.id)
     like.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
     @review = Review.find(params[:review_id])
     like = current_user.likes.find_by(review_id: @review.id)
     like.destroy
-    redirect_to request.referer
+    
+    #redirect_to request.referer
   end
 end
